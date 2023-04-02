4cb59143c9f692708a2688b77baed7f6aa0ae327
lowsky <lowsky@users.noreply.github.com>
2023-04-02 21:14:44 +0200

    add new, RFC 229, nextjs based promise handling approach

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import { Box, Heading, UnorderedList } from '@chakra-ui/react';
 import Head from 'next/head';
 
 import InternalLink from '../components/InternalLink';
@@ -14,7 +14,22 @@ export default function Index() {
 
             <Heading>What is it?</Heading>
 
-            <ul>
+            <Box>This is a demo of React.Suspense in data fetching scenarios.</Box>
+            <Box>
+                Main use case is typically showing a loading spinner.
+                <br />
+                Suspense helps to simplify the code structure for handling asynchronously data fetched even when it is
+                done incrementally.
+            </Box>
+
+            <Heading size="medium">Choose one of these different scenarios:</Heading>
+
+            <UnorderedList>
+                <li>
+                    🆕<InternalLink href={'/next'}>Experimental, RFC-229</InternalLink>{' '}
+                    <strong>use(await fetch)</strong> Fetching all data and use latest support of async data fetching
+                    via `const data = use(await fetch)`
+                </li>
                 <li>
                     <InternalLink href={'/restful'}>Old way</InternalLink> <strong>All or nothing:</strong> Fetching all
                     data in a top-level `useEffect()` + props-drilling
@@ -31,7 +46,7 @@ export default function Index() {
                     <InternalLink href={'/side-by-side'}>Side-by-Side</InternalLink> <strong>Comparison:</strong> Show
                     incrementally loading and wait-for-all
                 </li>
-            </ul>
+            </UnorderedList>
         </div>
     );
 }
